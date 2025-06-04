e90462b362aff54d09d1bec4d2786db9e55d53c6
liyiyuan <liyiyuan@pff.com>
2025-06-04 18:46:18 +0800

    feat(Setting):增加Agent的相关配置文件

--- Assets/Scripts/Editor/CreateSettingAsset.cs
+++ Assets/Scripts/Editor/CreateSettingAsset.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 public static class CreateSettingsAsset
 {
     [MenuItem("Assets/Create/FlowFieldPathfindingSetting/WorldSetting", false, 100)]
-    public static void CreateAsset()
+    public static void CreateWorldSettingAsset()
     {
-        WorldSetting settings = ScriptableObject.CreateInstance<WorldSetting>();
+        CreateAsset<WorldSetting>("WorldSetting");
+    }
+    
+    [MenuItem("Assets/Create/FlowFieldPathfindingSetting/AgentSetting", false, 100)]
+    public static void CreateAgentSettingAsset()
+    {
+        CreateAsset<AgentSetting>("AgentSetting");
+    }
+
+    static void CreateAsset<T>(string defaultName) where T : ScriptableObject
+    {
+        T settings = ScriptableObject.CreateInstance<T>();
         
-        string path = AssetDatabase.GenerateUniqueAssetPath("Assets/settings/WorldSetting.asset");
+        string path = AssetDatabase.GenerateUniqueAssetPath($"Assets/settings/{defaultName}_new.asset");
         AssetDatabase.CreateAsset(settings, path);
         AssetDatabase.SaveAssets();
         
--- Assets/Scripts/Setting/AgentSetting.cs
+++ Assets/Scripts/Setting/AgentSetting.cs
@@ -1,7 +1,22 @@
 using UnityEngine;
 
-public class WorldSetting : ScriptableObject
+public class AgentSetting : ScriptableObject
 {
-    public Vector2Int worldSize = new Vector2Int(20, 20);
-    public Vector3 cellSize = new Vector3(1.0f, 0.0f, 1.0f);
+    [Header("Normal Setting")]
+    public float radius = 0.5f; // 代理的半径
+    public float maxSpeed = 3.0f; // 代理的最大速度
+    public float mass = 1.0f; // 代理的质量
+    
+    [Header("Steering Behaviour : Arrive")]
+    [Range(1, 3)]
+    [Header("到达目标时的减速系数")]
+    public int arriveDeceleration = 2;
+    
+    [Header("Steering Behaviour : Wander")]
+    [Header("圆半径")]
+    public float wanderRadius = 1.0f;
+    [Header("圆距离")]
+    public float wanderDistance = 2.0f;
+    [Header("随机最大值")]
+    public float wanderJitter = 0.2f;
 }
